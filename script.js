64b2f8f4c98f6d47dbaf9fdab6ebe384dc1ede99
RaoRajat <126772200+RaoRajat@users.noreply.github.com>
2023-03-31 18:04:20 +0530

    Clock

---
+++ script.js
@@ -0,0 +1,16 @@
+
+setInterval(function(){
+
+let timeNow = new Date()
+let myTime = timeNow.toLocaleTimeString()
+
+document.querySelector('.time_container').innerHTML = myTime
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let myDate = timeNow.toLocaleDateString()
+document.querySelector('.date').innerHTML = timeNow.getDate()
+document.querySelector('.month').innerHTML = months[timeNow.getMonth()]
+document.querySelector('.year').innerHTML = timeNow.getFullYear()
+
+
+}, 1000)
